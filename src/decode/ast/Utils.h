15db640febf4d8f65eb2f64a1cef720d77b99775
dndanik <dndanik@gmail.com>
2018-01-12 17:59:49 +0300

    Check for null pointers in validation routines

--- src/decode/ast/Utils.h
+++ src/decode/ast/Utils.h
@@ -17,12 +17,14 @@ namespace decode {
 
 static const decode::Ast* findModule(const decode::Device* dev, bmcl::StringView name)
 {
+    if (!dev) {
+        return nullptr;
+    }
     auto it = std::find_if(dev->modules().begin(), dev->modules().end(), [name](const decode::Ast* module) {
         return module->moduleName() == name;
     });
     if (it == dev->modules().end()) {
         return nullptr;
-        //return "failed to find module " + wrapWithQuotes(name);
     }
     return *it;
 }
@@ -47,11 +49,9 @@ const T* findType(const decode::Ast* module, bmcl::StringView typeName)
     bmcl::OptionPtr<const decode::NamedType> type = module->findTypeWithName(typeName);
     if (type.isNone()) {
         return nullptr;
-        //return "failed to find type " + wrapWithQuotes(typeName);
     }
     if (type.unwrap()->typeKind() != decode::deferTypeKind<T>()) {
         return nullptr;
-        //return "failed to find type " + wrapWithQuotes(typeName);
     }
     return static_cast<const T*>(type.unwrap());
 }
@@ -65,33 +65,34 @@ static void expectFieldNum(Rc<const T>* container, std::size_t num)
     }
     if (t->fieldsRange().size() != num) {
         *container = nullptr;
-        //return "struct " + wrapWithQuotes(container->name()) + " has invalid number of fields";
+        return;
     }
 }
 
 template <typename T>
 static void expectField(Rc<const T>* container, std::size_t i, bmcl::StringView fieldName, const decode::Type* other)
 {
-    if (!other) {
-        return;
-    }
     const T* t = (*container).get();
     if (!t) {
         return;
     }
+    if (!other) {
+        *container = nullptr;
+        return;
+    }
     if (i >= t->fieldsRange().size()) {
         *container = nullptr;
-        //return "struct " + wrapWithQuotes(container->name()) + " is missing field with index " + std::to_string(i);
+        return;
     }
     const decode::Field* field = t->fieldAt(i);
     if (field->name() != fieldName) {
         *container = nullptr;
-        //return "struct " + wrapWithQuotes(container->name()) + " has invalid field name at index " + std::to_string(i);
+        return;
     }
     const decode::Type* type = field->type();
     if (!type->equals(other)) {
         *container = nullptr;
-        //return "field " + wrapWithQuotes(fieldName) + " of struct " + wrapWithQuotes(container->name()) + " is of invalid type";
+        return;
     }
 }
 
